6253f3a2e9e2b6a486e2c23c45450bbc7660f91f
Adith A Danthi <adith.danthi@gmail.com>
2021-01-16 14:07:42 +0530

    Firebase Setup

---
+++ src/utils/firebase.js
@@ -0,0 +1,23 @@
+import firebase from "firebase";
+import "firebase/auth"
+import "firebase/firestore"
+
+export const config = {
+    apiKey: process.env.VUE_APP_API_KEY,
+    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_APP_ID,
+    measurementId: process.env.VUE_APP_MEASUREMENT_ID
+}
+
+const firebaseApp = firebase.initializeApp(config);
+
+const auth = firebaseApp.auth();
+const db = firebaseApp.firestore();
+
+export default {
+    auth,
+    db
+}
