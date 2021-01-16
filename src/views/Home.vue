35e4e902debfa155171b76133f30778aa377fe68
Adith A Danthi <adith.danthi@gmail.com>
2021-01-16 20:49:27 +0530

    Added Login Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-app-bar
+        app
+    >
+      <v-img
+          alt="Developer Student Club"
+          class="shrink"
+          contain
+          src="@/assets/dsc.svg"
+          height="50"
+      />
+
+      <v-spacer></v-spacer>
+      <v-btn text>
+        Home
+      </v-btn>
+      <v-btn text>
+        About
+      </v-btn>
+      <v-btn text>
+        Chapter
+      </v-btn>
+      <v-btn text>
+        Events
+      </v-btn>
+      <v-btn
+          outlined
+          color="primary"
+          to="/login"
+      >
+        Sign In
+      </v-btn>
+    </v-app-bar>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,102 @@
 <template>
-$END$
+  <v-container
+      class="fill-height"
+      fluid
+  >
+    <v-card
+        class="align-center mx-auto pa-10"
+    >
+      <v-card-title>
+        <v-img
+            alt="Developer Student Club"
+            class="mx-auto"
+            contain
+            src="@/assets/dsc.svg"
+        />
+      </v-card-title>
+      <v-card-text>
+        <h2 class="mt-5 mb-10 font-weight-medium text-center" style="color: #2e2e2e">Login</h2>
+        <v-text-field
+            v-model="form.email"
+            type="email"
+            label="Email ID"
+            :rules="rules.email"
+            required
+            outlined
+        ></v-text-field>
+
+        <v-text-field
+            v-model="form.password"
+            :type="showPass ? 'text' : 'password'"
+            label="Password"
+            :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPass = !showPass"
+            hint="At least 8 characters"
+            :rules="[rules.required, rules.min]"
+            counter
+            required
+            outlined
+        ></v-text-field>
+        <v-row class="mt-3">
+          <v-col class="mt-0 pt-0">
+            <v-btn
+                class="text-capitalize"
+                text
+            >
+              Forgot Password?
+            </v-btn>
+          </v-col>
+          <v-col class="text-right mt-0 pt-0">
+            <v-btn
+                class="text-capitalize"
+                color="primary"
+                text
+                to="signup"
+            >
+              Register
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+            class="mx-auto pa-5 text-capitalize"
+            color="primary"
+            elevation="0"
+            @click="login"
+        >
+          Login
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 export default {
-name: "Login"
+  name: "Login",
+
+  data() {
+    const defaultForm = Object.freeze({
+      email: '',
+      pass: ''
+    });
+    return {
+      form: Object.assign({}, defaultForm),
+      showPass: false,
+      rules: {
+        email: [v => v.length > 0 || 'This field is required'],
+        required: value => !!value || 'Required.',
+        min: v => v.length >= 8 || 'Min 8 characters'
+      }
+    }
+  },
+
+  methods: {
+    login: function () {
+      alert('Login');
+    }
+  }
 }
 </script>
 
