43bae8f73c36ce86409472c375e6ca022e8663fe
Adith A Danthi <adith.danthi@gmail.com>
2021-01-18 13:36:13 +0530

    Created header Component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
+    <app-header></app-header>
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+import AppHeader from "../components/AppHeader";
+export default {
+  name: "About",
+  components: {
+    AppHeader
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,123 +1,20 @@
 <template>
   <div class="home">
-    <v-app-bar
-        app
-    >
-      <v-container class="py-0 fill-height">
-
-        <v-img
-            alt="Developer Student Club"
-            class="shrink"
-            contain
-            src="@/assets/dsc.svg"
-            height="50"
-        />
-
-        <v-spacer></v-spacer>
-
-        <v-btn
-            class="text-capitalize"
-            v-for="link in links"
-            :key="link"
-            text
-            :to="`/${link}`"
-        >
-          {{ link }}
-        </v-btn>
-
-        <v-menu
-            v-if="$store.getters.isLoggedIn"
-            bottom
-            rounded="lg"
-            offset-y
-        >
-          <template v-slot:activator="{ on }">
-            <v-btn
-                icon
-                x-large
-                v-on="on"
-            >
-              <v-avatar
-                  class="mx-5"
-                  color="grey darken-1"
-                  size="32"
-              >
-                <v-icon size="24" color="white">
-                  mdi-account-circle
-                </v-icon>
-              </v-avatar>
-            </v-btn>
-          </template>
-          <v-card class="pa-3">
-            <v-list-item-content class="justify-center">
-              <div class="mx-auto text-center">
-                <!--                <h3 class="mx-5">{{ currentUser.name }}</h3>-->
-                <!--                <p class="caption mt-1">-->
-                <!--                  {{ currentUser.email }}-->
-                <!--                </p>-->
-                <v-btn
-                    class="text-capitalize"
-                    to="/dashboard"
-                    depressed
-                    rounded
-                    text
-                >
-                  Dashboard
-                </v-btn>
-                <v-divider class="my-3"></v-divider>
-                <v-btn
-                    class="text-capitalize"
-                    @click="logout"
-                    depressed
-                    rounded
-                    text
-                >
-                  Logout
-                </v-btn>
-              </div>
-            </v-list-item-content>
-          </v-card>
-        </v-menu>
-        <v-btn
-            v-else
-            class="text-capitalize"
-            color="primary"
-            to="/login"
-            text
-        >
-          Sign In
-        </v-btn>
-      </v-container>
-    </v-app-bar>
+    <app-header></app-header>
   </div>
 </template>
 
 <script>
-
-import firebase from "../utils/firebase";
+import AppHeader from "../components/AppHeader";
 
 export default {
   name: 'Home',
+  components: {AppHeader},
   data: () => ({
-    links: [
-      'home',
-      'about',
-      'chapters',
-      'events',
-      'members'
-    ]
+
   }),
   methods: {
-    logout: function () {
-      firebase.auth.signOut()
-          .then(() => {
-            this.$store.commit('SET_LOGGED_IN', false);
-            this.$router.push({name: 'Login'});
-          })
-          .catch(err => {
-            console.log(err);
-          })
-    },
+
   }
 }
 </script>
